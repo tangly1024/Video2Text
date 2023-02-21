245213feafd6ed5450b36af6f58ddcc994fa7055
tangly1024.com <tlyong1992@hotmail.com>
2023-02-21 11:25:09 +0800

    提交requirement

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ from video_converter import convert_to_text
 
 if __name__ == '__main__':
     # 输入目标文件路径 for_example: r'/Users/xx/xxx.mp4'
-    source_media_path=r'[your_media_file_path]'
+    source_media_path = r'[your_media_file_path]'
     convert_to_text(source_media_path=source_media_path)
-
